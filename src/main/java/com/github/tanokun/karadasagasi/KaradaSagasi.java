aa512f5bb8b4ba27416abff992582b6d9ff3a70d
tanokun <azsxdcfvgbhnjmklqawsedrf0@gmail.com>
2021-04-25 21:03:26 +0900

    first-commit

---
+++ src/main/java/com/github/tanokun/karadasagasi/KaradaSagasi.java
@@ -0,0 +1,10 @@
+package com.github.tanokun.karadasagasi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KaradaSagasi extends JavaPlugin {
+
+    public void onEnable() { }
+
+    public void onDisable() { }
+}
