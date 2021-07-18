56f3155bbbe8c9eda2d13d744ff7106a0d7a3243
tanokun <azsxdcfvgbhnjmklqawsedrf0@gmail.com>
2021-07-18 14:27:52 +0900

    [add] location関係

---
+++ src/main/java/com/github/tanokun/karadasagasi/util/YamlUtils.java
@@ -0,0 +1,35 @@
+package com.github.tanokun.karadasagasi.util;
+
+import com.github.tanokun.karadasagasi.util.io.Config;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+
+public class YamlUtils {
+    public static void setLocation(Location location, Config config, String key) {
+        String locT = location.getWorld().getName() + " " + location.getX() + " " + location.getY() + " " + location.getZ() + " " + location.getPitch() + " " + location.getYaw();
+        config.getConfig().set(key, locT);
+    }
+
+    public static Location getLocation(Config config, String key) {
+        try {
+            String[] locT = config.getConfig().getString(key).split(" ");
+
+            World world = Bukkit.getWorld(locT[0]);
+            double x = Double.valueOf(locT[1]);
+            double y = Double.valueOf(locT[2]);
+            double z = Double.valueOf(locT[3]);
+
+            float p = Float.valueOf(locT[4]);
+            float y2 = Float.valueOf(locT[5]);
+
+            return new Location(world, x, y, z, p, y2);
+        }catch (Exception e) {
+            return new Location(Bukkit.getWorld("world"), 0, 0, 0);
+        }
+    }
+
+    public static String LocationToString(Location location) {
+        return Math.round(location.getX()) + " " + Math.round(location.getY()) +  " " + Math.round(location.getZ());
+    }
+}
