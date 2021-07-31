4029af63aa25a98c9a46af5eb2199f6e645ddfd7
tanokun <azsxdcfvgbhnjmklqawsedrf0@gmail.com>
2021-07-31 21:24:36 +0900

    [fix] スタート中の処理を追加

--- src/main/java/com/github/tanokun/karadasagasi/command/PlayerCommand.java
+++ src/main/java/com/github/tanokun/karadasagasi/command/PlayerCommand.java
@@ -2,12 +2,16 @@ package com.github.tanokun.karadasagasi.command;
 
 import com.github.tanokun.karadasagasi.KaradaSagasi;
 import com.github.tanokun.karadasagasi.game.GameRunnable;
+import com.github.tanokun.karadasagasi.game.State;
 import com.github.tanokun.karadasagasi.util.command.Command;
 import com.github.tanokun.karadasagasi.util.command.CommandContext;
+import com.github.tanokun.karadasagasi.util.scoreboard.FastBoard;
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.Arrays;
 
 public class PlayerCommand {
     @Command(
@@ -18,6 +22,11 @@ public class PlayerCommand {
     public void joinGame(CommandSender sender, CommandContext commandContext) {
         Player player = (Player) sender;
         GameRunnable gameRunnable = KaradaSagasi.getPlugin().getGameRunnable();
+
+        if (gameRunnable.getState() == State.GAMING) {
+            return;
+        }
+
         if (gameRunnable.getPlayers().contains(player)) {
             player.sendMessage(KaradaSagasi.PX + "既に参加しています");
             return;
@@ -37,6 +46,12 @@ public class PlayerCommand {
     public void QuitGame(CommandSender sender, CommandContext commandContext) {
         Player player = (Player) sender;
         GameRunnable gameRunnable = KaradaSagasi.getPlugin().getGameRunnable();
+
+        if (gameRunnable.getState() == State.GAMING) {
+            player.sendMessage(KaradaSagasi.PX + "既に始まっています");
+            return;
+        }
+
         if (!gameRunnable.getPlayers().contains(player)) {
             player.sendMessage(KaradaSagasi.PX + "参加していません");
             return;
@@ -46,4 +61,5 @@ public class PlayerCommand {
         KaradaSagasi.playSound(player, Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
         Bukkit.broadcastMessage(KaradaSagasi.PX + "§b" + player.getName() + "§bが参加を取り消しました §7[" + gameRunnable.getPlayers().size() + "/" + Bukkit.getOnlinePlayers().size() + "]");
     }
+
 }
