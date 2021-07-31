370d47dd1debbdd80329b9fab33cfc1637d94089
tanokun <azsxdcfvgbhnjmklqawsedrf0@gmail.com>
2021-07-31 21:23:28 +0900

    [add] ゲーム中のデータのTemp

---
+++ src/main/java/com/github/tanokun/karadasagasi/game/player/TempPlayer.java
@@ -0,0 +1,13 @@
+package com.github.tanokun.karadasagasi.game.player;
+
+public class TempPlayer {
+    private boolean die = false;
+
+    public boolean isDie() {
+        return die;
+    }
+
+    public void setDie(boolean die) {
+        this.die = die;
+    }
+}
