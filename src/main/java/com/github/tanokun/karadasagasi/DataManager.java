7aeedf199bb9c8550308ffdc6c12233195073142
tanokun <azsxdcfvgbhnjmklqawsedrf0@gmail.com>
2021-07-31 21:18:43 +0900

    [fix] 追加項目

--- src/main/java/com/github/tanokun/karadasagasi/DataManager.java
+++ src/main/java/com/github/tanokun/karadasagasi/DataManager.java
@@ -1,5 +1,6 @@
 package com.github.tanokun.karadasagasi;
 
+import com.github.tanokun.karadasagasi.util.YamlUtils;
 import com.github.tanokun.karadasagasi.util.io.Config;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -10,21 +11,22 @@ public class DataManager {
 
     private Location killerWaitingPosition;
 
+    private Location lobbyPosition;
+
     public DataManager(Plugin plugin) {
         dataFile = new Config("data.yml", plugin);
         dataFile.saveDefaultConfig();
-        String[] kwp = dataFile.getConfig().getString("killerWaitingPosition", "none").split(" ");
-        this.killerWaitingPosition = kwp[0].equals("none") ? Bukkit.getWorld("world").getSpawnLocation() :
-                new Location(Bukkit.getWorld(kwp[3]), Double.valueOf(kwp[0]), Double.valueOf(kwp[1]), Double.valueOf(kwp[2]));
-        if (!this.killerWaitingPosition.equals(Bukkit.getWorld("world").getSpawnLocation())) {
-            this.killerWaitingPosition.setPitch(Float.parseFloat(kwp[4]));
-            this.killerWaitingPosition.setYaw(Float.parseFloat(kwp[5]));
-        }
+
+        killerWaitingPosition = YamlUtils.getLocation(dataFile, "killerWaitingPosition");
+
+        lobbyPosition = YamlUtils.getLocation(dataFile, "lobbyPosition");
     }
 
     public void save(){
-        dataFile.getConfig().set("killerWaitingPosition", killerWaitingPosition.equals(Bukkit.getWorld("world").getSpawnLocation()) ? "none" :
-                killerWaitingPosition.getBlockX() + " " + killerWaitingPosition.getBlockY() + " " + killerWaitingPosition.getBlockZ() + " " + killerWaitingPosition.getWorld().getName() + " " + killerWaitingPosition.getPitch() + " " + killerWaitingPosition.getYaw());
+        YamlUtils.setLocation(killerWaitingPosition, dataFile, "killerWaitingPosition");
+
+        YamlUtils.setLocation(lobbyPosition, dataFile, "lobbyPosition");
+
 
         dataFile.saveConfig();
     }
@@ -33,7 +35,15 @@ public class DataManager {
         return killerWaitingPosition;
     }
 
+    public Location getLobbyPosition() {
+        return lobbyPosition;
+    }
+
     public void setKillerWaitingPosition(Location killerWaitingPosition) {
         this.killerWaitingPosition = killerWaitingPosition;
+    }
+
+    public void setLobbyPosition(Location lobbyPosition) {
+        this.lobbyPosition = lobbyPosition;
     }
 }
